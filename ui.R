4613f33f2da9440d0a4261dc5e94f137fd43a6cd
Megan McClarty <m_mcclarty@outlook.com>
2017-05-26 15:25:45 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -9,62 +9,57 @@ library(markdown)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(theme = "united",
-  tabsetPanel(
-    tabPanel("Upload First Data File",
-             sidebarLayout(
-               sidebarPanel(
-               #Choose and upload file from local machine
-                 fileInput('file1', 'Choose CSV File',
-                           accept=c('text/csv', 
-                                    'text/comma-separated-values,text/plain', 
-                                    '.csv')),
-               ),
-               mainPanel(
-                 h2('Selected Data'),
-                 p(),
-                 tableOutput('contents')
-               )
-             )
-    ),
-    tabPanel("Upload Second Data File",
-             pageWithSidebar(
-               headerPanel("Choose Second File"),
-               sidebarPanel(
-               ),
-               mainPanel(
-                 
-               )
-             )
-    ),
-    tabPanel("Join Data",
-             pageWithSidebar(
-               headerPanel("Hi"),
-               sidebarPanel(
-                 h3("Empty")
-               ),
-               mainPanel(
-                
-               )
-             )
-    ),
-    tabPanel("Plot Data",
-             pageWithSidebar(
-               headerPanel('My First Plot'),
-               sidebarPanel(
-                 
-                 # "Empty inputs" - they will be updated after the data is uploaded
-                 selectInput('xcol', 'X Variable', ""),
-                 selectInput('ycol', 'Y Variable', "", selected = "")
-                 
-               ),
-               mainPanel(
-                 plotOutput('MyPlot')
-                 htmlOutput("joinedplot")
-               )
-             )
-    )
-  
-        )
-
-  )
+                  tabsetPanel(
+                    tabPanel("Upload Data",
+                             sidebarLayout(
+                               sidebarPanel(
+                                 fileInput('file1', 'Choose CSV File',
+                                           accept=c('text/csv', 
+                                                    'text/comma-separated-values,text/plain', 
+                                                    '.csv')),
+                                 
+                                 # added interface for uploading data from
+                                 # http://shiny.rstudio.com/gallery/file-upload.html
+                                 tags$br(),
+                                 checkboxInput('header', 'Header', TRUE),
+                                 radioButtons('sep', 'Separator',
+                                              c(Comma=',',
+                                                Semicolon=';',
+                                                Tab='\t'),
+                                              ','),
+                                 radioButtons('quote', 'Quote',
+                                              c(None='',
+                                                'Double Quote'='"',
+                                                'Single Quote'="'"),
+                                              '"')
+                               ),
+                               mainPanel(
+                                 h2('Selected Data'),
+                                 p(),
+                                 tableOutput('contents')
+                               )
+                             )
+                    ),
+                    tabPanel("Plot Data",
+                             pageWithSidebar(
+                               headerPanel('Plot'),
+                               sidebarPanel(
+                                 radioButtons("choosePlot", "Choose style",
+                                              list("Scatter","Bar","Bubble")),
+                                 # "Empty inputs" - they will be updated after the data is uploaded
+                                 selectInput('xcol', 'X Variable', ""),
+                                 selectInput('ycol', 'Y Variable', "", selected = ""),
+                                 selectInput('id', 'ID Variable', "", selected = ""),
+                                 selectInput('colour','Colour Variable',"",selected=""),
+                                 textInput("title", "Plot Title", "Data Plot")
+                               ),
+                               mainPanel(
+                                 htmlOutput("joinedplot")
+                               )
+                             )
+                    )
+                    
+                  )
+                  
+)
 )
